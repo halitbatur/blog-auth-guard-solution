58f5485d658065e7875fb07925a9d195aa44d8d1
Wisam Naji <guitar90rocker@gmail.com>
2021-11-01 19:50:30 +0000

    initial commit

---
+++ server/routes/auth.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const User = require('./../models/user');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/signin', async (req, res) => {
+  const { username, password, rememberMe } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res
+      .status(400)
+      .render('user/signin', { error: 'Wrong username or password' });
+  }
+
+  const valid = await bcrypt.compare(password, user.password_hash);
+  if (!valid) {
+    return res
+      .status(400)
+      .render('user/signin', { error: 'Wrong username or password' });
+  }
+
+  res.setHeader('user', user.id);
+  req.user = user;
+  res.redirect('/user/authenticated');
+});
+
+router.post('/signup', async (req, res) => {
+  const {
+    firstname,
+    lastname,
+    username,
+    password,
+    password2,
+    acceptTos, // either "on" or undefined
+    avatar,
+  } = req.body;
+
+  // Check password quality
+  if (password !== password2) {
+    return res
+      .status(400)
+      .render('user/signup', { error: 'passwords do not match' });
+  }
+  // Check username is unique
+  let user = await User.findOne({ username });
+  if (user) {
+    return res
+      .status(400)
+      .render('user/signup', { error: `${username}: username already used` });
+  }
+
+  const password_hash = await bcrypt.hash(password, 10);
+
+  user = await User.create({
+    firstname,
+    lastname,
+    username,
+    avatar,
+    password_hash,
+  });
+
+  req.user = user;
+
+  res.redirect('/user/authenticated'); // this is only to exit tests, change on implementations
+});
+
+router.get('/signout', (req, res) => {
+
+});
+
+// renders sign up page
+router.get('/signup', (req, res) => {
+  res.render('user/signup');
+});
+
+// renders sign in page
+router.get('/signin', (req, res) => {
+  res.render('user/signin');
+});
+
+router.get('/authenticated', (req, res) => {
+  res.render('user/authenticated');
+});
+
+module.exports = router;
